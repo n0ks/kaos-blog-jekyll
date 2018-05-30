cc68231561438930081fa769e9f3e7043c51fd5c
n0ks <rodrigocmp29@gmail.com>
2018-05-30 15:03:28 -0300

    Assets Update

---
+++ assets/js/bundle.js
@@ -0,0 +1,1 @@
+const menuToggle=document.querySelector(".menu-toggle"),header=document.querySelector(".header");menuToggle.addEventListener("click",()=>{menuToggle.classList.toggle("header-open"),header.classList.toggle("is-open")});
