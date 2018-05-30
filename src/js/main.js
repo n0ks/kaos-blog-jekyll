b2e4d420e6e9257352fdf8e9b7685ca45e0f1959
n0ks <rodrigocmp29@gmail.com>
2018-05-30 15:04:38 -0300

    Mobile Menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
-console.log('xp');
+const menuToggle = document.querySelector('.menu-toggle')
+const header = document.querySelector('.header')
+
+menuToggle.addEventListener('click', () => {
+	menuToggle.classList.toggle('header-open')
+	header.classList.toggle('is-open')
+})
